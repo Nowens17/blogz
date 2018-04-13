586250c97744959484735df332555bcc7cfb631c
Nick Owens <nick_owens@rocketmail.com>
2018-04-12 21:11:51 -0500

    Committing updates to all files

--- main.py
+++ main.py
@@ -13,6 +13,27 @@ class Blog(db.Model):
     title = db.Column(db.String(120))
     body = db.Column(db.String(9999))
 
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+
+
+@app.route("/newpost", methods=['POST', 'GET'])
+def new_post():
+    blog_title = request.form['title']
+    blog_content = request.form['content']
+
+    return render_template("newpost.html")
+
+@app.route("/blog", methods=['POST', 'GET'])
+def blog_list():
+    blogs = Blog.query.all()
+    return render_template("blog.html", blogs=blogs)
+
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    return render_template("base.html")
 
 
 if __name__ == '__main__':
